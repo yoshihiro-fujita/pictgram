097cfa8cf7e3437c66b2276d0e13126447906bea
Yoshihiro Fujita <yoshixxx0010@gmail.com>
2020-07-12 20:51:20 +0900

    first commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     validates :email, presence: true
 
     has_secure_password
+
+    has_many :topics
+    has_many :favorites
+    has_many :favorite_topics, through: :favorites, source: 'topic'
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  get 'topics/new'
   get 'sessions/new'
   
   root 'pages#index'
   get 'pages/help'
 
-  resources :users
-
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
 end
 
